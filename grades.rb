12e96ccc2baade2e187a54fc7658f23e7735ccc6
steve <stevedanko1@gmail.com>
2013-12-01 18:36:17 -0500

    fixed while loop and refactored invalid command if statement.

--- grades.rb
+++ grades.rb
@@ -5,10 +5,8 @@ require_relative 'grade_summary_class'
 require_relative 'final_grade_class'
 require_relative 'write_to_file_class'
 
-
-require 'pry'
 require 'csv'
-binding.pry
+
 puts "Hi welcome to your grade book?"
 puts "To load your grades in type your file name"
 file_name = gets.chomp
@@ -21,7 +19,7 @@ puts "To view student averages type 'view student averages'"
 puts "To out put your grades to a file for report cards type 'report cards'"
 command = gets.chomp
 
-while command.downcase != 'done'
+while true
   if command == 'course analytics'
     grades = GradeSummary.new(file)
     grades.class_averages
@@ -47,6 +45,11 @@ while command.downcase != 'done'
     puts "Export Succesful"
     puts '========================'
     puts "What do you want to do? Type 'done' to exit"
+    command = gets.chomp
+  end
+  break if command == 'done'
+  if ['course analytics', "view student averages", 'report cards', 'done'].any? {|string| command.downcase != string }
+    puts "Invalid Command try again, type 'done' to exit"
     command = gets.chomp
   end
 end
